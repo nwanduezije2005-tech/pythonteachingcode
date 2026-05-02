cefa5268bd6daf9e294aaa9ee354d2150e175222
nwanduezije2005-tech <nwanduezije2005@gmail.com>
2026-05-01 23:14:32 -0400

    Update script1.py

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -22,9 +22,61 @@ def greet():
 def home():  
     return render_template("home.html",myName="Type your name in the box and click submit!")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+@app.route('/ezije/')
+def ezije():
+      '''
+This is a basic web page running with Flask.
+'''
+
+from flask import Flask, render_template, request
+#This is the Flask object, we are creating an instance of the Flask class and storing it in the variable app
+#__name__ is a special variable in Python that is the name of the module, which is this file's name without the .py extension
+app=Flask(__name__)
+
+#This is a decorator, it is a function that takes a function as a parameter!
+#A decorator is a function that wraps another function
+#This decorator is saying that when someone goes to the URL /greet, run the greet function
+@app.route('/greet', methods=['POST'])
+def greet():
+    inputName = request.form['myName']
+    ip = request.remote_addr
+    #write data to file or to DB
+    inputName = inputName.upper()+" hi!  Visiting from " + str(ip)
+    return render_template("home.html",myName=inputName)
+
+@app.route('/')
+def home():  
+    return render_template("home.html",myName="Type your name in the box and click submit!")
+
+@app.route('/ezije/')
+def ezije():
+    '''
+This is a basic web page running with Flask.
+'''
+
+from flask import Flask, render_template, request
+#This is the Flask object, we are creating an instance of the Flask class and storing it in the variable app
+#__name__ is a special variable in Python that is the name of the module, which is this file's name without the .py extension
+app=Flask(__name__)
+
+#This is a decorator, it is a function that takes a function as a parameter!
+#A decorator is a function that wraps another function
+#This decorator is saying that when someone goes to the URL /greet, run the greet function
+@app.route('/greet', methods=['POST'])
+def greet():
+    inputName = request.form['myName']
+    ip = request.remote_addr
+    #write data to file or to DB
+    inputName = inputName.upper()+" hi!  Visiting from " + str(ip)
+    return render_template("home.html",myName=inputName)
+
+@app.route('/')
+def home():  
+    return render_template("home.html",myName="Type your name in the box and click submit!")
+
+@app.route('/ezije')
+def ezije():
+    return "<title>Nwandu</title><h1>Nwandu</h1>"
 
 if __name__=="__main__":
     app.run(debug=True)
